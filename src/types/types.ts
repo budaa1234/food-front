2897d03d42891aab51e82dda49d2071d81bf4100
25LP4381 25LP4381 <25LP4381@G303-CC0138.local>
2025-07-01 18:11:23 +0800

    aggregate

---
+++ src/types/types.ts
@@ -0,0 +1,20 @@
+export type Category = {
+  categoryName: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+  _id: string;
+  foods: Food[]
+};
+
+export type Food = {
+  _id: string;
+  foodName: string;
+  price: number;
+  image: string;
+  ingredients: string;
+  category: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+};
