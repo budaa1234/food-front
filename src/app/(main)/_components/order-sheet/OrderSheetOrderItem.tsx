5ec08d345271bfa89e319431c4aac170eaee1b81
25LP4381 25LP4381 <25LP4381@pinecones-iMac.local>
2025-07-23 13:05:06 +0800

    order

--- src/app/(main)/_components/order-sheet/OrderSheetOrderItem.tsx
+++ src/app/(main)/_components/order-sheet/OrderSheetOrderItem.tsx
@@ -1,28 +1,37 @@
 import { Badge } from "@/components/ui/badge";
 import { Map, Soup, Timer } from "lucide-react";
+import { Order } from "./OrderSheetOrders";
 
-export const OrderSheetOrderItem = () => {
+export const OrderSheetOrderItem = (props: Order) => {
   return (
     <div className="space-y-3">
       <div className="flex item-center justify-between">
-        <h4 className="font-bold">$26.97 (#20156)</h4>
+        <h4 className="font-bold">{props.totalPrice}(#20156)</h4>
 
         <Badge variant="outline" className="border-red-500 rounded-full">
           Delivered
         </Badge>
       </div>
 
-      <div className="flex item-center justify-between">
-        <div className="flex item-center gap-2">
-          <Soup strokeWidth={1} size={16} />
-          <p className="text-muted-foreground text-xs">Sunshine Stackers</p>
-        </div>
-        <p className="text-muted-foreground text-xs">x 1</p>
+      <div className="flex item-center justify-between flex-col">
+        {props.foodOrderItems.map((orderItem) => {
+          return (
+            <div key={orderItem._id}>
+              <div className="flex item-center gap-2">
+                <Soup strokeWidth={1} size={16} />
+                <p className="text-muted-foreground text-xs">
+                  {orderItem.food.foodName}
+                </p>
+              </div>
+              <p className="text-muted-foreground text-xs">x {orderItem.quantity}</p>
+            </div>
+          );
+        })}
       </div>
 
       <div className="flex item-center gap-2">
         <Timer strokeWidth={1} size={16} />
-        <p className="text-muted-foreground text-xs">2024/12/24</p>
+        <p className="text-muted-foreground text-xs">{props.createdAt}</p>
       </div>
 
       <div className="flex item-center gap-2">
--- src/app/(main)/_components/order-sheet/OrderSheetOrders.tsx
+++ src/app/(main)/_components/order-sheet/OrderSheetOrders.tsx
@@ -1,7 +1,57 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { OrderSheetOrderItem } from ".";
+import { OrderSheet, OrderSheetOrderItem } from ".";
+import { useEffect, useState } from "react";
+
+type Food = {
+  _id: string;
+  foodName: string;
+  price: number;
+  image: string;
+  ingredients: string;
+  category: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+};
+
+export type Order = {
+  _id: string;
+  user: string;
+  totalPrice: number;
+  foodOrderItems: {
+    food: Food;
+    quantity: number;
+    price: string;
+    _id: string;
+  }[];
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+};
+type OrderResponse = {
+  success: boolean;
+  foodOrder: Order[];
+};
 
 export const OrderSheetOrders = () => {
+  const [orders, setOrders] = useState<Order[]>([]);
+  useEffect(() => {
+    const getCatgories = async () => {
+      const response = await fetch(
+        "http://localhost:4200/food-order/user/688056a64de455ef33ab0ce1"
+      );
+
+      const data = (await response.json()) as OrderResponse;
+
+      console.log(data);
+
+      setOrders(data.foodOrder);
+    };
+    getCatgories();
+  }, []);
+
+  console.log(orders?.[0]);
   return (
     <Card className="h-[87%]">
       <CardHeader className="p-4 ">
@@ -9,7 +59,9 @@ export const OrderSheetOrders = () => {
       </CardHeader>
 
       <CardContent className="p-4">
-        <OrderSheetOrderItem />
+        {orders.map((order) => {
+          return <OrderSheetOrderItem key={order._id} {...order} />;
+        })}
       </CardContent>
     </Card>
   );
